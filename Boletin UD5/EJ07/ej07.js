8609a292b7bf37549e4283ba24f1dc2759a07361
PatriciaRisGar <xprisgar115@ieshnosmachado.org>
2024-01-19 14:41:36 +0100

    [entornoCliente] - Boletin5 finalizado

--- Boletin UD5/EJ07/ej07.js
+++ Boletin UD5/EJ07/ej07.js
@@ -1,11 +1,11 @@
 const teclado = document.getElementById("teclado");
 
-teclado.addEventListener("clic", manejadorEvento);
+teclado.addEventListener("click", manejadorEvento);
 
 function manejadorEvento(event) {
-	if (event.target.tagName == "INPUT") {
-		const salida = document.getElementById("salida");
-		let digito = event.target.value;
-		salida.value += digito;
-	}
+  if (event.target.tagName == "INPUT") {
+    const salida = document.getElementById("salida");
+    let digito = event.target.value;
+    salida.value += digito;
+  }
 }
--- Boletin UD5/EJ10/ej10.js
+++ Boletin UD5/EJ10/ej10.js
@@ -1,1 +1,12 @@
+document.body.addEventListener("contextmenu", function (event) {
+	event.preventDefault();
+	document.getElementById("salida").innerHTML =
+		"Pulsado el boton derecho del boton ";
+});
 
+document.body.addEventListener(
+	"click",
+	() =>
+		(document.getElementById("salida").innerHTML =
+			"Pulsado el boton izquierdo del raton")
+);
